b443e881a4c0b390cee9178f6a5c114cff559cf1
gilosher93 <Mysc.gilosher@gmail.com>
2015-10-12 09:10:21 +0300

    add draw black circle

--- draw circle/draw circle/main.swift
+++ draw circle/draw circle/main.swift
@@ -20,7 +20,7 @@ func drawCircle(xPos:Int , yPos:Int, radius:Int){
         rowPrint = "";
         for j in 0...yPos*2{
             let distance = distanceFromPoints(Double(xPos), y1: Double(yPos) , x2: Double(i), y2: Double(j));
-            rowPrint += distance - Double(radius) <= 1.4 && distance - Double(radius) >= 0.7  ? star : space;
+            rowPrint += distance - Double(radius) <= 1.3 && distance - Double(radius) >= 0.7  ? star : space;
             rowPrint += space;
         }
         print(rowPrint);
@@ -46,11 +46,46 @@ func drawWhiteCircle(xPos:Int , yPos:Int, radius:Int){
     }
 }
 
+func drawBlackCircle(xPos:Int , yPos:Int, radius:Int){
+    if(radius >= xPos || radius >= yPos){
+        print("רדיוס גדול מידי")
+        return;
+    }
+    var rowPrint:String;
+    let star = "*";
+    let space = " ";
+    for i in 0...xPos*2{
+        rowPrint = "";
+        for j in 0...yPos*2{
+            let distance = distanceFromPoints(Double(xPos), y1: Double(yPos) , x2: Double(i), y2: Double(j));
+            rowPrint += distance - Double(radius) <= 0.8 ? star : space;
+            rowPrint += space;
+        }
+        print(rowPrint);
+    }
+}
+
+func printCanvas(var canvas:[[Bool]]){
+    var rowPrint:String;
+    let star = "*";
+    let space = " ";
+    for i in 0..<canvas.count{
+        rowPrint = "";
+        for j in 0..<canvas[i].count{
+            rowPrint += canvas[i][j] ? star : space;
+            rowPrint += space;
+        }
+        print(rowPrint);
+    }
+}
+
 func distanceFromPoints(x1:Double , y1:Double, x2:Double, y2:Double)->Double{
     let deltaX = x1-x2;
     let deltaY = y1-y2;
     return sqrt(deltaX*deltaX + deltaY*deltaY);
     
 }
+
 drawWhiteCircle(10, yPos: 10, radius: 8)
+drawBlackCircle(10, yPos: 10, radius: 8)
 drawCircle(10, yPos: 10, radius: 8)
