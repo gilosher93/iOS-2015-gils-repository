a52575892c30ce29b72d1442f7a25f4b29d1abe9
gilosher93 <Mysc.gilosher@gmail.com>
2015-10-29 12:35:03 +0200

    UISegmentedControl to switch screens

--- UISegmentedControl to switch screens/UISegmentedControl to switch screens/ViewController.swift
+++ UISegmentedControl to switch screens/UISegmentedControl to switch screens/ViewController.swift
@@ -9,10 +9,45 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    var segmentedControl: UISegmentedControl!;
+    var containers = [UIView]();
+    let margin: CGFloat = 5;
+    let segments = ["screen 1" , "screen 2" , "screen 3" , "screen 4"];
+    
     override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        super.viewDidLoad();
+        
+        createSegmentedControl();
+        createContainer(0);
+    }
+    
+    func segmentedControlValueChanged(sender: UISegmentedControl){
+        let index = sender.selectedSegmentIndex;
+        for i in 0..<segments.count{
+            containers[i].hidden = i != index;
+        }
+    }
+    
+    func createSegmentedControl(){
+        segmentedControl = UISegmentedControl(items: segments);
+        segmentedControl.frame = CGRect(x: 5, y: 20, width: view.frame.width - 10, height: 30);
+        segmentedControl.addTarget(self, action: "segmentedControlValueChanged:", forControlEvents: UIControlEvents.ValueChanged);
+        view.addSubview(segmentedControl);
+    }
+    
+    func createContainer(selected: Int){
+        segmentedControl.selectedSegmentIndex = selected
+        for i in 0..<segments.count{
+            let container = UIView(frame: CGRect(x: margin, y: segmentedControl.frame.maxY, width: segmentedControl.frame.width, height: view.frame.height - (segmentedControl.frame.maxY + margin) - margin));
+            containers.append(container);
+            container.hidden = i != selected;
+            let label = UILabel(frame: CGRect(x: 0, y: 0, width: 200, height: 30));
+            label.text = segments[i];
+            label.font = UIFont.boldSystemFontOfSize(20); //font size
+            container.addSubview(label)
+            view.addSubview(container);
+        }
     }
 
     override func didReceiveMemoryWarning() {
